930026cbdeecdda6ea4225a420ea4115e14f48bb
Joel Maciel <soldado25461@gmail.com>
2022-11-26 15:17:30 -0300

    studying data shares

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = 'Joel Maciel';
+  userData = {
+    email: 'joel@maciel.com',
+    role: 'admin',
+    age: 39,
+  };
   title = 'course_angular';
 }
